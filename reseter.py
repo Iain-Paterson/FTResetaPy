72cac19ff8ab1490c97de39998e2a1ca54d741b2
Jenkins FTDI <jenkins@ftdichip.com>
2019-10-18 16:59:35 +0100

    some Progress

--- reseter.py
+++ reseter.py
@@ -1,4 +1,6 @@
 # reseter
+import sys
+import ftd2xx as ftd
 import logging
 import threading
 import time
@@ -18,6 +20,7 @@ def init_reseter():
 
 class FTReseter:
     """ FTReseter class...."""
+    device = None
     state = resetState.INIT
     trigger = False
     reset_period = 10 # seconds
@@ -27,60 +30,72 @@ class FTReseter:
 
     def __init__(self):
         """ Constructor goes here """
-        state = resetState.INIT
-        trigger = False
-        sm = {  resetState.INIT: FTReseter.init,
+        self.state = resetState.INIT
+        self.trigger = False
+        self.sm = {  resetState.INIT: FTReseter.init,
                 resetState.RESET: FTReseter.reset,
                 resetState.SET:   FTReseter.set
          }
-        logging.basicConfig(format=format, filename= 'log.log', level=logging.INFO, datefmt="%H:%M:%S")
+        #logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+        print("__init__")
 
     def set_period (self, p):
-        reset_period = p
+        self.reset_period = p
 
     def set_dwell(self, d):
-        reset_dwell = d
+        self.reset_dwell = d
 
 
     def init(self):
         # init initialise the output pins 
-        log.info("init.")
+        logging.info("init.")
+        
         try:
-            state = SET
-            d = ftd.open(0)    # Open first FTDI device
-            di = d.getDeviceInfo() 
+            self.state = resetState.SET
+            try:
+                self.device = ftd.open(0)    # Open first FTDI device
+            except:
+                self.device = None
+                return
+
+            di = self.device.getDeviceInfo() 
 
             OP = 0x01            # Bit mask for output D0
-            d.setBitMode(OP, 1)  # Set pin as output, and async bitbang mode
-            d.write(str(OP))     # Set output high
-            d.write(str(0))      # Set output low
+            self.device.setBitMode(OP, 1)  # Set pin as output, and async bitbang mode
+            self.device.write(str(OP))     # Set output high
+            self.device.write(str(0))      # Set output low
         except:
             logging.info("Exception")
-            state = INIT
-            d.close()
+            self.state = resetState.INIT
+            if self.device is not None:
+                self.device.close()
         else:
-            state = SET    
-
+            self.state = resetState.SET    
+            self.device.close()
         time.sleep(2)
         return    
 
     def reset(self):
+        self.device.write(str(0))      # Set output low
+        time.sleep(0.05) # pull reset low for 50 ms
         logging.info("reset.")
         #reset
 
-        state = Set   
+        self.state = resetState.Set   
         time.sleep(2)
         return
 
     def set(self):        
+        self.device.write(str(OP))     # Set output high
         logging.info("set.")
-        if trigger:  
-            state = RESET
+        if self.trigger:  
+            self.state = resetState.RESET
         
     def reseter_state_machine_run(self):
-        loggin.info("reseter_state_machine_run")
+        logging.info("reseter_state_machine_run")
 
 
 
 if __name__ == "__main__":
     init_reseter()
+    logging.info('Exiting reserer.py main')
